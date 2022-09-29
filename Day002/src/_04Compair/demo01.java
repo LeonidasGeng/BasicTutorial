fd24c34baa08926a60e6156aaa35b6888ddb3e2d
Leonidas <leonidas.geng@outlook.com>
2022-09-29 16:54:52 -0400

    9/29/2022

---
+++ Day002/src/_04Compair/demo01.java
@@ -0,0 +1,17 @@
+package _04Compair;
+/*
+ ==等于 !=不等于 >大于 <小于 >=大于等于 <=小于等于
+ 结果一定是布尔类型boolean, true 条件成立，false 条件不成立
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = 11;
+        boolean result1 = a == b;
+        boolean result2 = a == c;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println("a和b相等吗？ " + (a>b));
+    }
+}
