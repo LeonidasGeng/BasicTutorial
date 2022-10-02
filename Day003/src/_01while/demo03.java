9d34580135af6eb0f4e6fe067661eafc0a04817f
Leonidas <leonidas.geng@outlook.com>
2022-10-02 19:42:39 -0400

    Update demo03.java

--- Day003/src/_01while/demo03.java
+++ Day003/src/_01while/demo03.java
@@ -9,6 +9,20 @@ package _01while;
  */
 public class demo03 {
     public static void main(String[] args) {
+        //1，定义变量保存纸的厚度
+        double paper = 0.0001;
 
+        //2，定义变量保存山的高度
+        int mount = 8848;
+
+        //3，定义一个变量保存纸张对折次数
+        int count =0;
+        //3，编写循环条件
+        while(paper <= mount){
+            //现在纸张厚度=原本纸张厚度 * 2
+            paper = paper *2;
+            count ++;
+        }
+        System.out.println("对折次数为： "+ count);
     }
 }
