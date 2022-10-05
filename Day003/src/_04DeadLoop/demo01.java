2a2a88644f13daf6da58c490ee1b6eb247e0b2e7
Leonidas <leonidas.geng@outlook.com>
2022-10-04 21:28:02 -0400

    Create demo01.java

---
+++ Day003/src/_04DeadLoop/demo01.java
@@ -0,0 +1,24 @@
+package _04DeadLoop;
+
+/*
+死循环：
+无限次循环
+
+死循环写法：
+for
+
+while
+
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        //for 循环的死循环
+        for(;;){
+            System.out.println("Hello World");
+        }
+
+        //while 循环的死循环                                                        (true){
+            System.out.println("Hello Worldtr5666666666666666666666666666666666666666666666666666666666666666");
+        }
+    }
+}
