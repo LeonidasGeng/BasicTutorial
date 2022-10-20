b1443737e0a2c370b4295eecc580f9cd0e6f247c
Leonidas <leonidas.geng@outlook.com>
2022-10-20 13:33:32 -0400

    202210201333

---
+++ Day004/src/_02Opreation/demo06.java
@@ -0,0 +1,26 @@
+package _02Opreation;
+
+import java.util.Random;
+
+/*
+有以下数组：int[] arr = new int[5];
+随机产生5个1-100的整数，储存到数组中
+打印每个值
+ */
+public class demo06 {
+    public static void main(String[] args) {
+        //1,定义一个数组，长度为5
+        int[] arr=new int[5];
+        //2,随机产生5个随机数
+        Random r = new Random();
+        for (int i = 0; i < 5; i++) {
+            int rn = r.nextInt(100)+1;
+            arr[i]=rn;
+        }
+        //3,并且存储到数组中
+        System.out.print("这5个数分别是: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+",");
+        }
+    }
+}
---
+++ Day004/src/_03Case/_01Case1.java
@@ -0,0 +1,32 @@
+package _03Case;
+
+import java.util.Scanner;
+
+/*
+输入10个同学的成绩，输出他们的最高分，最低分，平均成绩
+ */
+public class _01Case1 {
+    public static void main(String[] args) {
+        //1,定义一个数组，长度为10，为同学的成绩
+        int[] arr = new int[10];
+        //2,输入每个学生的成绩
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("请输入第"+(i+1)+"同学的成绩");
+            //录入学生的成绩储存到num这个变量中
+            int num =sc.nextInt();
+            arr[i]=num;
+        }
+        //3,求平均成绩
+        //累加所有人的成绩
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        //平均成绩
+        System.out.println("这些人的平均成绩为："+(sum/arr.length));
+        //3,最高分，最低分
+        
+
+    }
+}
