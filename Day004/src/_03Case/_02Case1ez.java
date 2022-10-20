cff6c63c6703ea17eebc0d6a331e12230b468e66
Leonidas <leonidas.geng@outlook.com>
2022-10-20 14:00:38 -0400

    202210201400

--- Day004/src/_03Case/_02Case1ez.java
+++ Day004/src/_03Case/_02Case1ez.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 /*
 输入10个同学的成绩，输出他们的最高分，最低分，平均成绩
+减少遍历次数，减少代码编写
  */
-public class _01Case1 {
+public class _02Case1ez {
     public static void main(String[] args) {
         //1,定义一个数组，长度为10，为同学的成绩
         int[] arr = new int[10];
@@ -20,25 +21,23 @@ public class _01Case1 {
         //3,求平均成绩
         //累加所有人的成绩
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum+=arr[i];
-        }
-        //平均成绩
-        System.out.println("这些人的平均成绩为："+(sum/arr.length));
         //3,最高分，最大值
         int max = arr[0];
+        int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
             if (arr[i]>max){
                 max = arr[i];
             }
-        }
-        //4,最低分，最小值
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
             if (arr[i]<min){
                 min = arr[i];
             }
         }
+        //平均成绩
+        System.out.println("这些人的平均成绩为："+(sum/arr.length));
+        System.out.println("这些人的最高分为："+max);
+        System.out.println("这些人的最低分为："+min);
+
 
     }
 }
---
+++ Day004/src/_03Case/_03Case2.java
@@ -0,0 +1,28 @@
+package _03Case;
+
+import java.util.Random;
+
+/*
+双色球,6个红球，1个蓝球
+1，6个红球，1-33之间
+2，1个蓝球，1-16之间
+3，暂时不考虑重复，排序问题
+ */
+public class _03Case2 {
+    public static void main(String[] args) {
+        int[] redArray = new int[6];
+        Random r = new Random();
+        for (int i = 0; i < 6; i++) {
+            int redNum = r.nextInt(33)+1;
+            redArray[i] = redNum;
+
+        }
+        int blueNum = r.nextInt(16)+1;
+
+        System.out.println("本期开奖号码为：");
+        for (int i = 0; i < redArray.length; i++) {
+            System.out.print(redArray[i]+",");
+        }
+        System.out.print(blueNum);
+    }
+}
